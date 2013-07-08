b896d263891a0da458dc830780229618c4b0b0fc
Dorota Celińska <dcelinska@ibe.edu.pl>
2013-07-08 15:51:36 +0200

    nowe zapytania w inne i szkoly_uczniowie

--- zapytania/inne.sql
+++ zapytania/inne.sql
@@ -1,1 +1,9 @@
 -- ZAPYTANIA, KTORYCH KATEGORIE TRUDNO OKRESLIC
+
+--srednia liczba uczniow uczestniczacych w placowce w zajeciach przygotowanie do zycia w rodzinie 
+select avg(uczniowie_zyc) from (select sum(jednkl_zycrodzog) as uczniowie_zyc, jedn_id from sio2012.jednklasa_ucz group by jedn_id) q;
+
+--srednia liczba klas, w ktorych prowadzono przedmiot przygotowanie do zycia w rodzinie/placowke
+select avg(klasa_zyc) from (select count(klasa_id) as klasa_zyc, jedn_id from sio2012.jednklasa_ucz group by jedn_id) q;
+
+
--- zapytania/szkoly_uczniowie.sql
+++ zapytania/szkoly_uczniowie.sql
@@ -11,6 +11,19 @@ sio=> select avg(liczba_ucz) from (select jedn_id, sum(jednnaboddzkl_brokucz) as
 --liczba szkol, w ktorych bylo wiecej uczniow niz srednia z calej proby
 select count (jedn_id) from sio2012.jednnaboddzkl_uczbrok having sum(jednnaboddzkl_brokucz) > (select avg(liczba_ucz) from (select jedn_id, sum(jednnaboddzkl_brokucz) as liczba_ucz from sio2012.jednnaboddzkl_uczbrok group by jedn_id) pomocnicza);
 
+--srednia liczba osob powtarzajacych rok w placowkach
+select avg(powtarzajacy) from (select sum(jednnabkl_uczbrokpowtarz) as powtarzajacy, jedn_id from sio2012.jednnabkl_uczog group by jedn_id) q;
+
+--najwieksza liczba powtarzajacych rok uczniow w jednostce
+select query1.powtarzajacy
+from (select sum(jednnabkl_uczbrokpowtarz) as powtarzajacy, jedn_id from sio2012.jednnabkl_uczog group by jedn_id) query1,
+(select max(powtarzajacy) as maksimum from (select sum(jednnabkl_uczbrokpowtarz) as powtarzajacy, jedn_id from sio2012.jednnabkl_uczog group by jedn_id) query2) query3 where query1.powtarzajacy=query3.maksimum; i
+
+--najwieksza liczba indywidualnie nauczanych w placowce
+select query1.ind
+from (select sum(jednnabkl_uczbrokindywnau) as ind, jedn_id from sio2012.jednnabkl_uczog group by jedn_id) query1,
+(select max(ind) as maksimum from (select sum(jednnabkl_uczbrokindywnau) as ind, jedn_id from sio2012.jednnabkl_uczog group by jedn_id) query2) query3 where query1.ind=query3.maksimum;
+
 --Znajdz szkoly, w ktorych na dzien badania bylo mniej uczniow niz na koniec poprzedniego roku
 
 create view uczniowie1 as select jednnaboddzkl_uczbrok.jedn_id, sum(jednnaboddzkl_brokucz) as brok, sum(jednnaboddzkl_prokucz) as prok from sio2012.jednnaboddzkl_uczbrok, sio2012.jednnaboddzkl_uczprok where jednnaboddzkl_uczbrok.jedn_id=jednnaboddzkl_uczprok.jedn_id group by jednnaboddzkl_uczbrok.jedn_id;
